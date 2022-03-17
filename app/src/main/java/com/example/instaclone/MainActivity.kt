0b1e40b49cc20b2f3e18d86949feedba75b89e4d
mirandov <tremors224@gmail.com>
2022-03-17 14:36:11 +0300

    MirandovTest: first commit

--- app/src/main/java/com/example/instaclone/MainActivity.kt
+++ app/src/main/java/com/example/instaclone/MainActivity.kt
@@ -6,7 +6,7 @@ import com.example.instaclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+//    My commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
